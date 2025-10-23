fe905f3bc39b05b243ee1aaa6b7d530a3a718bee
Aswin Reji <aswinreji.004@gmail.com>
2025-10-23 07:48:59 +0000

    fix: issue related to not running in sandbox fixed by making safe check of script tag and used window.console.log and use map to join the string without breaking the code

--- src/components/sandBox/sandBox.jsx
+++ src/components/sandBox/sandBox.jsx
@@ -1,38 +1,51 @@
 import React, { useEffect, useRef, useState } from 'react'
 
 const useSandBox = ({ code: initialCode }) => {
-    const iframeRef = useRef(null)
-    const [output, setOutput] = useState("")
-    const runCode = (codeToRun = initialCode) => {
-        setOutput("")
-        const script = `
-        <script>
-        const log = (...args) => parent.postMessage({type:'console', message:args.join(' ') }, '*');
-        console.log = log;
-        try {
-        ${codeToRun}
-        } catch (err){
-         log('Error:',err.message);
-         }
-        <\/script> 
-        `;
-        iframeRef.current.srcdoc = script;
-    }
-    useEffect(() => {
-        const listener = (event) => {
-            if (event.data && event.data.type === "console") {
+  const iframeRef = useRef(null)
+  const [output, setOutput] = useState("")
+  const runCode = (codeToRun = initialCode) => {
+    setOutput("")
+    console.log(codeToRun, 'code to run is here')
+    const safeCode = codeToRun.replace(/<\/script>/g, "<\\/script>");
 
-                setOutput((prev) => prev + event.data.message + "\\n");
-            }
-        }
-        window.addEventListener("message", listener``);
-        return () => window.removeEventListener("message", listener)
-    }, []);
-    return {
-        runCode,
-        output,
-        iframe: <iframe ref={iframeRef} sandbox='allow-scripts' style={{ display: "none" }}></iframe>
+    const script = `
+    <script>
+      // Redirect console.log
+      window.console.log = (...args) => {
+        parent.postMessage(
+          { type: 'console', message: args.map(a => String(a)).join(' ') },
+          '*'
+        );
+      };
+
+      try {
+        ${safeCode}
+      } catch (err) {
+        console.log('Error:', err.message);
+      }
+    <\/script>`;
+    iframeRef.current.srcdoc = script;
+
+  }
+  useEffect(() => {
+    console.log('inside useEffect')
+    const listener = (event) => {
+      console.log(event, 'event')
+      if (event.data && event.data.type === "console") {
+        console.log(event.data.message, 'event message')
+        setOutput((prev) => prev + event.data.message + "\n");
+      } else {
+        console.log('error is here')
+      }
     }
+    window.addEventListener("message", listener);
+    return () => window.removeEventListener("message", listener)
+  }, []);
+  return {
+    runCode,
+    output,
+    iframe: <iframe ref={iframeRef} sandbox='allow-scripts' style={{ display: "none" }}></iframe>
+  }
 }
 
 export default useSandBox
