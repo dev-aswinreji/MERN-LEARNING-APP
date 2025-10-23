37e9cc33b5f236ab63db46f2468ace38027e1c60
Aswin Reji <aswinreji.004@gmail.com>
2025-10-23 08:03:37 +0000

    fix: fixed ui or monaco editor with correct height and width but we need to maually assign height for each lessons cuz the code structure is different for each lessons and utilize each space

--- src/components/monacoEditor/monacoEditor.jsx
+++ src/components/monacoEditor/monacoEditor.jsx
@@ -1,10 +1,11 @@
 import { Editor } from '@monaco-editor/react'
 import React from 'react'
 
-const MonacoEditor = ({ code, onChange, readonly = false, height = "250px" }) => {
+const MonacoEditor = ({ code, onChange, readonly = false, height = "150px" ,width= "550px"}) => {
     return (
         <Editor
             height={height}
+            width={width}
             defaultLanguage='javascript'
             value={code}
             onChange={onChange}
